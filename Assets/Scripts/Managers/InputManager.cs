3d2a86145a4f70936cb1c74a602381cf75ddace8
pumpkinLKB <aosxn2@naver.com>
2022-10-11 22:35:34 +0900

    202210111

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    public Action KeyAction = null;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnUpdate()
+    {
+        if (Input.anyKey == false) return;
+
+        if(KeyAction != null)
+        {
+            KeyAction.Invoke();
+        }
+    }
+}
